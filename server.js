cc1a5d80df4cd0d932d9d870cc4a7a2e8f776a02
Devon Harris <dbharris2@gmail.com>
2017-03-05 09:48:19 -0800

    [fitbit] Improve app skeleton

---
+++ server.js
@@ -0,0 +1,11 @@
+var config = require( './config/app.json' );
+var express = require('express');
+var fs = require( 'fs' );
+var path = require('path');
+
+var app = express();
+app.set('port', (process.env.PORT || 8080));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
