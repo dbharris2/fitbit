971ae1ae1451c1675a96d9f2422bb95f0c931de1
Devon Harris <dbharris2@gmail.com>
2017-03-05 09:48:19 -0800

    [fitbit] Improve app skeleton

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,35 @@
 var path = require('path');
 var webpack = require('webpack');
- 
-module.exports = {
-  entry: './main.js',
-  output: { path: __dirname, filename: 'bundle.js' },
+
+var BUILD_DIR = path.resolve(__dirname, 'public/bin');
+var JSX_DIR = path.resolve(__dirname, 'public/jsx');
+var APP_ENTRY = JSX_DIR + '/main.jsx';
+
+var config = {
+  entry: {
+    app: APP_ENTRY,
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
   module: {
     loaders: [
       {
         test: /.jsx?$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
+        include: JSX_DIR,
         query: {
           presets: ['es2015', 'react']
         }
       }
     ]
   },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    root: JSX_DIR,
+    modulesDirectories: ['node_modules']
+  }
 };
+
+module.exports = config;
