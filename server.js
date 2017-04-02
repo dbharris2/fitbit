8e117abe8853e15190b9f03a9248f9b99d01e230
Devon Harris <dbharris2@gmail.com>
2017-04-01 18:15:20 -0700

    [fitbit] Create profile endpoint

--- server.js
+++ server.js
@@ -19,33 +19,36 @@ createJsonEndpoints(app, '/app', APP_FILE);
 var client;
 
 app.get('/authenticate', function(req, res) {
-  fs.readFile(APP_FILE, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-
-    const appData = JSON.parse(data);
-
-    client = new FitbitApiClient(appData.client_id, appData.client_secret);
-
-    const authorizeUrl = client.getAuthorizeUrl(
+  createFitbitApiClient(function(fitbitApiClient) {
+    const authorizeUrl = fitbitApiClient.getAuthorizeUrl(
       'activity heartrate location nutrition profile settings sleep social',
       FITBIT_AUTHORIZATION_CALLBACK_URL
     );
-
     res.redirect(authorizeUrl);
   });
 });
 
 app.get('/fitbit-callback', function(req, res) {
-  client.getAccessToken(req.query.code, FITBIT_AUTHORIZATION_CALLBACK_URL).then(function(result) {
-    // TODO: Store the access token and redirect to a valid url
-    console.log(JSON.stringify(result));
-    console.log('Access token: ' + result.access_token);
-    res.redirect('/');
-  }).catch(function(error) {
-    res.send(error);
+  createFitbitApiClient(function(fitbitApiClient) {
+    fitbitApiClient.getAccessToken(req.query.code, FITBIT_AUTHORIZATION_CALLBACK_URL).then(function(result) {
+      // TODO: Store the access token and redirect to a valid url
+      console.log(JSON.stringify(result));
+      console.log('Access token: ' + result.access_token);
+      res.redirect('/');
+    }).catch(function(error) {
+      res.send(error);
+    });
+  });
+});
+
+app.get('/profile', function(req, res) {
+  createFitbitApiClient(function(fitbitApiClient) {
+    fetchJson(ACCESS_TOKEN_FILE, function(jsonData) {
+      fitbitApiClient.get('/profile.json', jsonData.access_token).then(function(results) {
+        const data = results[0];
+        res.json(data);
+      });
+    });
   });
 });
 
@@ -53,15 +56,28 @@ app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
 
+function createFitbitApiClient(onCreate) {
+  fetchJson(APP_FILE, function(appData) {
+    const client = new FitbitApiClient(appData.client_id, appData.client_secret);
+    onCreate(client);
+  });
+}
+
 function createJsonEndpoints(app, apiPath, filePath) {
   app.get(apiPath, function(req, res) {
-    fs.readFile(filePath, function(err, data) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      } else {
-        res.json(JSON.parse(data));
-      }
+    fetchJson(filePath, function(jsonData) {
+      res.json(jsonData);
     });
+  });
+}
+
+function fetchJson(filePathToJson, onFetch) {
+  fs.readFile(filePathToJson, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    } else {
+      onFetch(JSON.parse(data));
+    }
   });
 }
