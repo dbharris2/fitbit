56695dd4fa78ed376f03acdab359ba994ad788ba
Devon Harris <dbharris2@gmail.com>
2017-04-03 20:56:23 -0700

    [fitbit] Migrate onto async/await

--- jsx/client.jsx
+++ jsx/client.jsx
@@ -1,9 +1,9 @@
-const FitbitApiClient = require('fitbit-node')
+import FitbitApiClient from 'fitbit-node'
+
+require('babel-polyfill')
 
 /**
  * Fetches data from Fitbit
- *
- * TODO: Use promises rather than callbacks
  */
 export default class FitbitClient {
 
@@ -15,36 +15,33 @@ export default class FitbitClient {
    * See {@link https://dev.fitbit.com/docs/oauth2/#access-token-request Fitbit Access Token Request}
    * for more information
    */
-  getAccessToken(code, callbackUrl, onResult, onError) {
-    this.client.getAccessToken(code, callbackUrl)
-      .then((result) => {
-        console.log(result)
-        this._setAccessTokenInfo(result)
-        onResult(result)
-      })
-      .catch((error) => { onError(error) })
+  async getAccessToken(code, callbackUrl) {
+    const accessTokenInfo = await this.client.getAccessToken(code, callbackUrl)
+    this.setAccessTokenInfo(accessTokenInfo)
+    return accessTokenInfo
   }
 
   /**
    * See {@link https://dev.fitbit.com/docs/activity/#get-daily-activity-summary Fitbit Activity Documentation}
    * for more information
    */
-  getActivity(date, onResult) {
-    this.client
+  async getActivity(date, onResult) {
+    const activity = await this.client
       .get('/activities/date/' + date + '.json', this.accessToken, this.userId)
-      .then((result) => { onResult(result[0]) })
+    return activity[0]
   }
 
   /**
    * See {@link https://dev.fitbit.com/docs/activity/#activity-time-series Fitbit Activity Time Series Documentation}
    * for more information
    */
-  getActivityTimeSeries(resourcePath, baseDate, endDate, onResult) {
-    this.client.get(
+  async getActivityTimeSeries(resourcePath, baseDate, endDate, onResult) {
+    const activityTimeSeries = await this.client.get(
       '/' + resourcePath + '/date/' + baseDate + '/' + endDate + '.json',
       this.accessToken,
       this.userId,
-    ).then((result) => { onResult(result[0]) })
+    )
+    return activityTimeSeries[0]
   }
 
   /**
@@ -62,9 +59,13 @@ export default class FitbitClient {
    * See {@link https://dev.fitbit.com/docs/user/#get-profile Fitbit Profile Documentation}
    * for more information
    */
-  getProfile(onResult) {
-    this.client.get('/profile.json', this.accessToken, this.userId)
-      .then((result) => { onResult(result[0]) })
+  async getProfile() {
+    const profile = await this.client.get(
+      '/profile.json',
+      this.accessToken,
+      this.userId,
+    )
+    return profile[0]
   }
 
   /**
@@ -73,12 +74,12 @@ export default class FitbitClient {
    */
   refreshAccessToken() {
     this.client.refreshAccessToken(accessToken, refreshToken, -1)
-      .then((result) => { this._setAccessTokenInfo(result) })
+      .then((result) => { this.setAccessTokenInfo(result) })
   }
 
-  _setAccessTokenInfo(result) {
-    this.accessToken = result.access_token
-    this.refreshToken = result.refresh_token
-    this.userId = result.user_id
+  setAccessTokenInfo(accessTokenInfo) {
+    this.accessToken = accessTokenInfo.access_token
+    this.refreshToken = accessTokenInfo.refresh_token
+    this.userId = accessTokenInfo.user_id
   }
 }
