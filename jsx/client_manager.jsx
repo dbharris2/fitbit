20e075750b9f4142d28a328506467dfb6decb606
Devon Harris <dbharris2@gmail.com>
2017-04-17 18:31:17 -0700

    [fitbit] Add tom to team 2

--- jsx/client_manager.jsx
+++ jsx/client_manager.jsx
@@ -30,7 +30,7 @@ function getTeamTwoCompetitors(
     return competitor.profile.user.encodedId === '2XCGBN' ||
       competitor.profile.user.encodedId === '4C7CC5' ||
       competitor.profile.user.encodedId === '2WRBR6' ||
-      competitor.profile.user.encodedId === '4CBZRP' ||
+      competitor.profile.user.encodedId === '5MSHQS' ||
       competitor.profile.user.encodedId === '4BPPZQ';
   });
 }
